4397541abd7903db2e05971c5965e9f3318a5cfb
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-23 01:50:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School 
+  school = School.new("Bayside High School")
+    school.add_student("Zach Morris", 9)
+      school.roster
+      
+      hash["new_key"] = [ ]
+      hash["new_key"] << "new_value_for_value_array"
+      
+      hash
+    end
+  end
